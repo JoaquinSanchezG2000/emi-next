0a248cd40c503a666c07e101662e214b6419c547
joaquin <joaquinsanchezg@hotmail.com>
2022-09-12 11:24:24 -0300

    commit 9 border map

--- pages/Componentes/Maps.jsx
+++ pages/Componentes/Maps.jsx
@@ -26,9 +26,10 @@ const Maps = props => {
           </li>
         </ul>
       </div>
-      <div className="map__container w-[100%] border border-black mt-10 ">
+      <div className="map__container w-[100%] border-double border-[10px] border-green-600  mt-10 ">
         <div className="m-1  ">
-          <iframe className="w-full h-[20rem]  "
+          <iframe
+            className="w-full h-[22rem]  "
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25168.731484564927!2d-57.796785759973694!3d-37.951650962222985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95852efdfd3420cf%3A0xdb2005c1cd0d533e!2sSierra%20de%20los%20Padres%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1662991383663!5m2!1ses!2sar"
             style={{ border: 0 }}
             loading="lazy"
